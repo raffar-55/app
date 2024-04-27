b2b4c7163186b3dc2aaa1d533424e741100a17f5
LENOVO <rfrf30515@gmail.com>
2024-04-27 22:16:24 +0500

    1.1

--- resources/views/app/nav.blade.php
+++ resources/views/app/nav.blade.php
@@ -1,1 +1,16 @@
-<?php
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Navbar">
+    <div class="container-xl">
+        <a class="navbar-brand" href="{{ route('home') }}">App</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbar">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('players.index') }}">players</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
--- resources/views/app/player.blade.php
+++ resources/views/app/player.blade.php
@@ -1,1 +1,69 @@
-<?php
+<div class="fw-semibold bg-white border rounded p-3">
+    <div class="row g-2">
+        <div class="col-12 fs-4">
+            {{ $obj->title }}
+        </div>
+        <div class="col-3 text-truncate">
+            <span class="text-secondary">User:</span>
+            <span class="{{ isset($f_user) ? 'mark':'' }}">
+                {{ $obj->user->name }}
+            </span>
+        </div>
+        <div class="col-3 text-truncate">
+            <span class="text-secondary">League:</span>
+            <span class="{{ isset($f_league) ? 'mark':'' }}">
+                {{ $obj->league->name }}
+            </span>
+        </div>
+        <div class="col-3">
+            <span class="text-secondary">Position:</span>
+            <span class="{{ count($f_positions) > 0 ? 'mark':'' }}">
+                {{ $obj->position->name }}
+            </span>
+        </div>
+      <!--  <div class="col-3">
+            <span class="text-secondary">Credit:</span>
+            @if($obj->credit)
+                <span class="text-success {{ $f_credit ? 'mark':'' }}">Yes</span>
+            @else
+                <span class="text-danger">No</span>
+            @endif -->
+        </div>
+        <div class="col-3 text-truncate">
+            <span class="text-secondary">Country:</span>
+            <span class="{{ isset($f_country) ? 'mark':'' }}">
+                {{ $obj->location->name }}
+            </span>
+        </div>
+        <div class="col-3">
+            <span class="text-secondary">Club:</span>
+            <span class="{{ isset($f_leagueClub) ? 'mark':'' }}">
+                {{ $obj->leagueClub->name }}
+            </span>
+        </div>
+        <div class="col-3">
+            <span class="text-secondary">Experience:</span>
+            <span class="{{ count($f_experiences) > 0 ? 'mark':'' }}">
+                {{ $obj->experience->name }}
+            </span>
+        </div>
+      <!--  <div class="col-3">
+            <span class="text-secondary">Exchange:</span>
+            @if($obj->exchange)
+                <span class="text-success {{ $f_exchange ? 'mark':'' }}">Yes</span>
+            @else
+                <span class="text-danger">No</span>
+            @endif-->
+        </div>
+        <div class="col-3">
+            <span class="text-secondary">Date:</span> {{ $obj->created_at->format('d.m.Y') }}
+        </div>
+        <div class="col-9">
+            <span class="text-secondary">Price:</span>
+            <span class="{{ (isset($f_minPrice) or isset($f_maxPrice)) ? 'mark':'' }}">{{ number_format($obj->price, 2, '.', ' ') }} <small>TMT</small></span>
+        </div>
+        <div class="col-12">
+            <span class="text-secondary">Description:</span> {{ $obj->body }}
+        </div>
+    </div>
+</div>
