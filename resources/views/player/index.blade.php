acdcb83a5c3ced4b0895f73befda72708bd318cf
LENOVO <rfrf30515@gmail.com>
2024-04-27 22:16:06 +0500

    1.1

--- resources/views/player/index.blade.php
+++ resources/views/player/index.blade.php
@@ -1,1 +1,31 @@
-<?php
+@extends('layouts.app')
+@section('title')
+    All players
+@endsection
+@section('content')
+    <div class="container py-4">
+        <div class="row g-4">
+            <div class="col-md-4 col-xl-3">
+                @include('app.filter')
+            </div>
+            <div class="col">
+                <div class="row g-3">
+                    @forelse($objs as $obj)
+                        <div class="col-12">
+                            @include('app.player')
+                        </div>
+                    @empty
+                        <div class="col-12">
+                            <div class="fs-1 text-center rounded border p-5">
+                                Player not found :(
+                            </div>
+                        </div>
+                    @endforelse
+                </div>
+                <div class="pt-4">
+                    {{ $objs->links() }}
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
