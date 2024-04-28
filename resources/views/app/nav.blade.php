dcf7e08af9b84b4db09e4d10d803403fbb02ba79
LENOVO <rfrf30515@gmail.com>
2024-04-28 17:31:05 +0500

    1.1

--- resources/views/app/nav.blade.php
+++ resources/views/app/nav.blade.php
@@ -8,8 +8,18 @@
         <div class="collapse navbar-collapse" id="navbar">
             <ul class="navbar-nav me-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('players.index') }}">players</a>
+                    <a class="nav-link" href="{{ route('players.index') }}">Countries</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('players.index') }}">Leagues</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('players.index') }}">LeagueClubs</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('players.index') }}">Players</a>
+                </li>
+
             </ul>
         </div>
     </div>
